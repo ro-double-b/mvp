7bffe035ff1ca1eb7f65802ffcf6aa952f43818c
ro-double-b <robb.marsee@gmail.com>
2016-07-11 16:50:02 -0700

    update selection page where it registers which queen was selected and passes it into a variable...to set up the backend now

--- client/app/controllers/selectController.js
+++ client/app/controllers/selectController.js
@@ -1,4 +1,9 @@
 angular.module('fantasyDragRace.select', [])
 .controller('SelectController', function($scope, $window) {
   // will fill this out at a later point if I have enough time...
+  $scope.selection = 'hello there'
+  $scope.newSelection = 'try this again'
+  $scope.submitTest = function(target) {
+    $scope.selection = target
+  }
 })
