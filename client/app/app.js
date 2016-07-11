253e9c0d355864e90d8a27195519550d4e8cad46
ro-double-b <robb.marsee@gmail.com>
2016-07-11 15:29:01 -0700

    finish linking each of the different pages together.

--- client/app/app.js
+++ client/app/app.js
@@ -9,6 +9,26 @@ angular.module('fantasyDragRace', ['ngRoute', 'fantasyDragRace.auth'])
     templateUrl: 'app/views/login.html',
     controller: 'AuthController'
   })
+  .when('/rules', {
+    templateUrl: 'app/views/rules.html',
+    controller: 'AuthController'
+    // controller to be updated
+  })  
+  .when('/home', {
+    templateUrl: 'app/views/home.html',
+    controller: 'AuthController'
+    // controller to be updated
+  })
+  .when('/selection', {
+    templateUrl: 'app/views/selection.html',
+    controller: 'AuthController'
+    // controller to be updated
+  })
+  .when('/results', {
+    templateUrl: 'app/views/results.html',
+    controller: 'AuthController'
+    // controller to be updated
+  })
   .otherwise({
     redirectTo: '/'
   })
