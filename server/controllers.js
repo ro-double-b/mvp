b2074b205140eb5d85ea494919dfab424ca19855
ro-double-b <robb.marsee@gmail.com>
2016-07-12 13:38:51 -0700

    update the server controllers with sudocode

--- server/controllers.js
+++ server/controllers.js
@@ -1,23 +1,45 @@
 var schemas = require('./db/models.js')
 
 module.exports = {
-  postLogin: function(req, res) {
+  postLogin: function(req, res) { // function handling when you click the sign in button
+    // check to see if the user is in the database
+      // if so checks to see if the password matches what is in the database
+        // if so responds back with a success
+
+      // will also have to send to the global scope that you are said user
+
+    // else sends back an error back to the login page
    console.log('you got into this post login request!')   
   },
 
-  getLogin: function(req, res) {
+  // getLogin: function(req, res) {
+  //   console.log('you got into this get login request')
+  // },
+
+  postSignup: function(req, res) { // function handling when you click a sign up button
+    // check to see if the user has been taken before
+      // if not then will submit to the database
+
+      // will also have to send to the global scope that you are said user
+
+      // else it will respond back to the login page
     console.log('you got into this get login request')
   },
 
-  postSignup: function(req, res) {
-    console.log('you got into this get login request')
-  },
+  postSelection: function(req, res) { // function handling when you click the select button
+    // checks to see who the user is
+      // check to make sure that the user along with that week number has not been submitted
+        // if none both of those are true, sends information to the database
 
-  postSelection: function(req, res) {
+      // else just updates said entry
+    // else sends back that noone is signed in and cannot make selection
     console.log('you got into this post selection reqeust')
   },
 
   getSelection: function(req, res) {
+    // whenever the results page is rendered, do a get request for all of the data
+
+    // organize the data and render it to the title based on the filter function
     console.log('you got into this get selection request!')
   }
 }
