c13ffbbba08a7503e7578491a5c86c6a6687adfa
Robb Marsee <robb.marsee@gamil.com>
2016-07-12 01:52:30 -0700

    "updated schemas to include primary keys for each schema"

--- server/db/models.js
+++ server/db/models.js
@@ -1,24 +1,41 @@
 var mongoose = require('mongoose')
 
 var userSchema = new mongoose.Schema({
-  user_id: Number,
+  user_id: {
+    type: Number,
+    unique: true
+  },
   user_name: String,
   user_password: String
 })
 
 var queenSchema = new mongoose.Schema({
-  queen_id: Number,
+  queen_id: {
+    type: Number,
+    unique: true
+  },
   queen_name: String
 })
 
 var winSchema = new mongoose.Schema({
-  week_id: Number,
+  week_id: {
+    type: Number,
+    unique: true
+  },
   win_queen_id: Number
 })
 
 var selectionSchema = new mongoose.Schema({
-  selection_id: Number,
+  selection_id: {
+    type: Number,
+    unique: true
+  },
   selection_user_id: Number,
   selection_queen_id: Number,
   selection_week_id: Number
 })
+
+mongoose.model('User', userSchema)
+mongoose.model('Queen', queenSchema)
+mongoose.model('Win', winSchema)
+mongoose.model('Selection', selectionSchema)
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 // var methodOverride = require('methodOverride')
 var mongoose = require('mongoose')
 mongoose.connect('mongoose://localhose:27102/db')
+require('./db/models.js')
 
 var port = 3000
 
