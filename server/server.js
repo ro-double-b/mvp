83d97820e2333d2e89c08255080f99018a74fd0e
Robb Marsee <robb.marsee@gamil.com>
2016-07-12 01:30:18 -0700

    "left comments on the api requests for clarity which ones have been enabled to recieve them"

--- server/server.js
+++ server/server.js
@@ -11,23 +11,23 @@ app.use(express.static(__dirname + '/../client'))
 
 // existing users
 app.post('/api/users/login', function(request, response) {
-  console.log('testin you just sent a post request buddy')
+  console.log('testin you just sent a post request buddy: signin')
 })
 app.get('/api/users/login', function(request, response) {
-
+  // nothing makes a request to this route yet
 })
 // new users
 app.post('/api/users/signup', function(request, response) {
-
+  console.log('testin you just sent a post request buddy: signup')
 })
 
 // select queen
 app.post('/api/selection', function(request, response) {
-
+  // nothing makes a request to this route yet
 })
 // display results
 app.get('/api/selection', function(request, response) {
-
+  // nothing makes a request to this route yet
 })
 
 app.listen(port);
