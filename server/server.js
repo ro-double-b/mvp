cba15eaf31f00317e8c50adf754c04c6ec31cb77
Robb Marsee <robb.marsee@gamil.com>
2016-07-11 23:46:30 -0700

    "added routes to server"

--- server/server.js
+++ server/server.js
@@ -9,7 +9,26 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../client'))
 
-// require('./routes')(app, express)
+// existing users
+app.post('/api/users/login', function(request, response) {
+
+})
+app.get('/api/users/login', function(request, response) {
+
+})
+// new users
+app.post('/api/users/signup', function(request, response) {
+
+})
+
+// select queen
+app.post('/api/selection', function(request, response) {
+
+})
+// display results
+app.get('/api/selection', function(request, response)) {
+
+}
 
 app.listen(port);
 console.log('Your server is listening on port: ' + port)
